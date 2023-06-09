25837d85ee32b881b24b374140d9b1cdfaa81023
Abbass <abbassmortazavi@gmail.com>
2023-06-09 11:59:00 +0330

    add fields in users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->text('image')->nullable();
+            $table->text('description')->nullable();
+            $table->text('location')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
