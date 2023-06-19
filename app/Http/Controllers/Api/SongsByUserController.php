23d50d063997f87f2016eb713c62a8d21d61a01b
Abbass <abbassmortazavi@gmail.com>
2023-06-19 23:33:47 +0330

    get all songs

---
+++ app/Http/Controllers/Api/SongsByUserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\User\UserService;
+use Exception;
+use Illuminate\Http\Request;
+
+class SongsByUserController extends Controller
+{
+    public function __construct(protected UserService $service)
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     * @throws Exception
+     */
+    public function index(int $userId)
+    {
+        try {
+            $user = $this->service->show($userId);
+            return response()->json($user->songs);
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+            return response()->json($exception->getMessage());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\SongController;
+use App\Http\Controllers\Api\SongsByUserController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('songs', [SongController::class, 'store']);
     Route::delete('delete/song/{id}/{user_id}', [SongController::class, 'destroy']);
+    Route::get('user/{user_id}/songs', [SongsByUserController::class, 'index']);
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
