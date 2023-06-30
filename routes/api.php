e61681c4392564759c0904d395ec2b2baa31d4ec
Abbass <abbassmortazavi@gmail.com>
2023-06-30 20:18:28 +0330

    fix bug api

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,9 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('posts', 'index');
     });
 
+    Route::post('logout', [AuthController::class, 'logout']);
+
+
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
